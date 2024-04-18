be105049c71bf091415c1ddfd27b93988b3df60b
gabi1482 <gabi.stanguini14@gmail.com>
2024-04-18 00:44:32 -0300

    Criação do Formulario de Cadastro da View e o DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using CP2_.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CP2_.NET.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+
+        {
+        }
+
+        public DbSet<User> MVC_USUARIOS { get; set; }
+    }
+}
+
+
+
