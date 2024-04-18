e1107318a230278a0e172fd21fed4ce9700c6699
Nreis280 <nicolasreissanto@gmail.com>
2024-04-18 00:57:07 -0300

    Criando as Migrations

---
+++ Migrations/20240418035614_first.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CP2_.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MVC_USUARIOS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    UserName = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UserEmail = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UserPassword = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MVC_USUARIOS", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MVC_USUARIOS");
+        }
+    }
+}
