29cad4c66bf5620e215ed939efa21d15d8b5addc
gabi1482 <gabi.stanguini14@gmail.com>
2024-04-18 02:10:51 -0300

    Adicionamos a View do UserLogin

---
+++ Views/UserLogin/FLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CP2_.NET.Views.Login
+{
+    public class FLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
