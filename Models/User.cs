d54750ab81cca22cecf9d86d6318cbf4c86292e3
Alberto <albertoperuchi11@gmail.com>
2024-04-18 00:23:11 -0300

    Model User

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,11 @@
-﻿namespace CP2_.NET.Models
-{
+﻿namespace CP2_.NET.Models;
+using System.ComponentModel.DataAnnotations;
+
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPassword { get; set; } = string.Empty;
     }
-}
